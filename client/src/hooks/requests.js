6c4f67f161fbde4144285e459261de86b8682379
Stefan Mocek Polska <etktuzudu@gmail.com>
2023-02-23 18:04:33 +0100

    addNewLaunch added to frontend

--- client/src/hooks/requests.js
+++ client/src/hooks/requests.js
@@ -17,13 +17,19 @@ async function httpGetLaunches() {
 
 // Submit given launch data to launch system.
 async function httpSubmitLaunch(launch) {
-  await fetch(`${API_URL}/launches`, {
-    method: 'POST',
-    headers:{
-      "Content-type": "application/json"
-    },
-    body: JSON.stringify(launch)
-  })
+  try {
+    return await fetch(`${API_URL}/launches`, {
+      method: 'POST',
+      headers:{
+        "Content-type": "application/json"
+      },
+      body: JSON.stringify(launch)
+    });
+  } catch (error) {
+    return {
+      ok: false
+    }
+  }
 }
 
 async function httpAbortLaunch(id) {
