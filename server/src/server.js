d330f633b5d4f9f7e5ad336cbf7b3d91d286385d
Stefan Mocek Polska <etktuzudu@gmail.com>
2023-02-24 21:15:32 +0100

    connecting to mongo

--- server/src/server.js
+++ server/src/server.js
@@ -1,4 +1,6 @@
+require('dotenv').config();
 const http = require('http');
+const mongoose = require('mongoose');
 
 const app = require('./app')
 const {loadPlanetsData} = require('./models/planets.model')
@@ -7,7 +9,18 @@ const PORT = process.env.PORT || 5000
 
 const server = http.createServer(app);
 
+const url = process.env.MONGO_URL
+
+mongoose.connection.once('open', () => {
+  console.log('Connection ready!');
+});
+
+mongoose.connection.on('error', (err) => {
+  console.error(err)
+})
+
 async function startServer () {
+  await mongoose.connect(url);
   await loadPlanetsData();
   server.listen(PORT, () => {
     console.log(`Server is listening on port: ${PORT}`);
